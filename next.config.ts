fb777ad9fd365406befe6c9fd4429952ae477e75
wahfei <wahfeilee@yahoo.com>
2026-07-28 17:05:44 +0800

    update csp

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  images: {
-    qualities: [70, 75],
-  }
+	/* config options here */
+	reactCompiler: true,
+	images: {
+		qualities: [70, 75],
+	},
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: [
+							"default-src 'self'",
+							"script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+							"style-src 'self' 'unsafe-inline'",
+							"img-src 'self' https://image.mux.com data:",
+							"media-src 'self' https://stream.mux.com",
+							"connect-src 'self' https://stream.mux.com https://inferred.litix.io",
+							"frame-src 'self' https://player.mux.com",
+						].join("; "),
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
