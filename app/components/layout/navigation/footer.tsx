13bcec8184da6c9df3c52ecf11caff4db6ea7c98
wahfei <wahfeilee@yahoo.com>
2026-08-02 17:34:06 +0800

    add: social media links

--- app/components/layout/navigation/footer.tsx
+++ app/components/layout/navigation/footer.tsx
@@ -32,8 +32,8 @@ export default function Footer() {
 			<div className="flex gap-5 items-center max-[1390px]:gap-1 max-[1390px]:flex-col max-[1390px]:justify-center max-[1390px]:items-center">
 				<p className="mb-0! max-md:text-center max-sm:text-[12px]!">Let&apos;s Look Deeper. Plays into our theme of all-seeing, and our eye logo.</p>
 				<div className="flex items-center justify-center gap-5 max-sm:mt-4">
-					<InstagramIcon link={"#"} />
-					<LinkedinIcon link={"#"} />
+					<InstagramIcon link={"https://www.instagram.com/juno.magic"} />
+					<LinkedinIcon link={"https://www.linkedin.com/company/themagicmakers"} />
 				</div>
 			</div>
 		</footer>
